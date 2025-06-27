fe44e9093b827991a2c82c85136f074675f79754
Vinícius Alves Pires <viniccius.alves.pires@gmail.com>
2025-06-27 12:19:04 -0300

    Fix: retirada de redundancia das pastas static

--- django-proj/vnsblog/settings.py
+++ django-proj/vnsblog/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",  # or os.path.join(BASE_DIR, "static")
+    f"{BASE_DIR}/../static"
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
